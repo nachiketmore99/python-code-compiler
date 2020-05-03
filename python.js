62a89ea5e61b3e4e1d650264195a5164095335ad
Nachiket More <nachiketmore99@gmail.com>
2020-05-03 20:29:24 +0530

    make it better

---
+++ python.js
@@ -0,0 +1,7 @@
+// document.getElementById('run').addEventListener('click', function(){
+//     var spawn = require('child_process').spawn;
+//     var process = spawn('python', ['./aplifile.py']);
+//     process.stdout.on('data', data => {
+//         console.log(data.toString());
+//     });
+// });
